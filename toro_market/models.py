0ec286616d86bdc9e5598d1fc37cfc4b08adb4e0
ivoreda <ivoredafej@gmail.com>
2023-11-10 11:13:13 +0100

    fixed deployment crashing issue

--- toro_market/models.py
+++ toro_market/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+# create a model for energy product
+# one to one relationship to product from user/business
+
+"""
+id
+project name
+project description
+energy capacity
+energy source
+location
+price
+image1 
+image2
+
+created_at
+updated_at
+"""
+
+
+"""
+only businesses can create energy product listing
+
+
+
+"""
--- toro_market/views.py
+++ toro_market/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+"""
+create views to handle CRUD operations on the projects
+
+
+"""
